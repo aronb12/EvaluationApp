d98e46ee574e4596499e97f6ce16d1ebb5bc23cd
aronb12 <arona12@ru.is>
2014-03-17 16:29:55 +0000

    updated index.html

--- src/app.js
+++ src/app.js
@@ -1,20 +1,39 @@
 var app = angular.module("EvaluationApp", ["ngRoute"]);
 
-app.config(function($routeProvider) {
-	$routeProvider.when("/", {
-		templateUrl: "templates/login.html",
-		controller: "LoginController"
-	}).when("/admin", {
-		templateUrl: "templates/admin.html",
-		controller: "AdminController"
-	}).when("/home", {
-		templateUrl: "templates/home.html",
-		controller: "HomeController"
-	}).when("/evaluation/:evaluationID", {
-		templateUrl: "templates/evaluation.html",
-		controller: "EvaluationController"
-	}).when("/evaluation/", {
-		templateUrl: "templates/evaluation.html",
-		controller: "EvaluationController"
-	}).otherwise({ redirectTo: "/"});
-});
+app.config(['$routeProvider', 
+	function($routeProvider) {
+		$routeProvider
+		.when("/", {
+			templateUrl: "templates/login.html",
+			controller: "LoginController"
+		}).when("/admin", {
+			templateUrl: "templates/admin.html",
+			controller: "AdminController",
+			resolve: {
+				this: function($location, LoginFactory){
+					if(LoginFactory.getRole() !== 'admin'){
+						$location.path('/');
+					}
+				}
+			}
+		}).when("/admin", {
+			templateUrl: "templates/admin.html",
+			controller: "AdminController",
+			resolve: {
+				this: function($location, LoginFactory){
+					if(LoginFactory.getRole() !== 'admin'){
+						$location.path('/');
+					}
+				}
+			}
+		}).when("/home", {
+			templateUrl: "templates/home.html",
+			controller: "HomeController"
+		}).when("/evaluation/:evaluationID", {
+			templateUrl: "templates/evaluation.html",
+			controller: "EvaluationController"
+		}).when("/evaluation/", {
+			templateUrl: "templates/evaluation.html",
+			controller: "EvaluationController"
+		}).otherwise({ redirectTo: "/"});
+}]);
