7dd6f70b5f6bb9b0196ec202877ddc1fea7d9e32
aronb12 <arona12@ru.is>
2014-03-17 21:43:15 +0000

    updated eval.js

--- src/services/eval.js
+++ src/services/eval.js
@@ -13,10 +13,7 @@ app.factory('EvalFactory', [
 
 				$http.get('http://dispatch.ru.is/h22/api/v1/evaluationtemplates/' + id, {})
 				.success(function(data, status, headers){
-					//var evaluations = data;
-					console.log(data);
-
-					deferred.resolve("data");
+					deferred.resolve(data);
 				})
 				.error(function(){
 					deferred.reject(status);
@@ -42,46 +39,12 @@ app.factory('EvalFactory', [
 			createEvaluation: function(titleIS, titleEN, introTextIS, introTextEN, courseQuestions, teacherQuestions){
 				//POST api/v1/evaluationtemplates
 
-				id = this.getEvaluations();
-				console.log(id.length);
-
-				/*var deferred = $q.defer();
-
-				var courseQuestions = [{
-						ID: 1,
-						TextIS: 'Spurning 1',
-						TextEN: 'Question 1',
-						ImageURL: 'No image',
-						Type: 'Type of question?',
-						Answers: [{
-							ID: 1,
-							TextIS: 'Svar 1',
-							TextEN: 'Answer 1',
-							ImageURL: 'why does an answer need an image?',
-							Weight: 5
-						}]
-					}];
-				var teacherQuestions = [{
-						ID: 1,
-						TextIS: 'kennaraspurning 2',
-						TextEN: 'teacher question 2',
-						ImageURL: 'picture?',
-						Type: 'type of question',
-						Answers: [{
-							ID: 1,
-							TextIS: 'svar 1',
-							TextEN: 'Answer 1',
-							ImageURL: 'mynd?',
-							Weight: 5
-						}]
-					}];
-
+				var deferred = $q.defer();
 				$http.post('http://dispatch.ru.is/h22/api/v1/evaluationtemplates', {
-					ID: 2,
-					TitleIS: 'prufa2',
-					TitleEN: 'test2',
-					IntroTextIS: 'Þetta er prufa 2',
-					IntroTextEN: 'Learn Icelandic!',
+					TitleIS: titleIS,
+					TitleEN: titleEN,
+					IntroTextIS: introTextIS,
+					IntroTextEN: introTextEN,
 					CourseQuestions: courseQuestions,
 					TeacherQuestions: teacherQuestions
 				})
@@ -92,11 +55,13 @@ app.factory('EvalFactory', [
 					deferred.reject(status);
 				});
 
-				return deferred.promise;*/
+				return deferred.promise;
 
 			},
-			activateEvaluation: function(){
+			activateEvaluation: function(id){
 				//calls /api/vi/evaluations - POST
+				
+
 			}
 		};
 	}
