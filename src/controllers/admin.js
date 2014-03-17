f397a59ce3ba7e44b3dfe84a2c9ae599664b6e7d
aronb12 <arona12@ru.is>
2014-03-17 14:25:47 +0000

    updated controllers

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,9 +1,17 @@
 app.controller("AdminController", [
-	"$scope", "$http", "$location", "LoginFactory",
-	function($scope, $http, $location, LoginFactory){
-		
-
+	"$scope", "$http", "$location", "LoginFactory", "EvalFactory",
+	function($scope, $http, $location, LoginFactory, EvalFactory){
+		$scope.token = LoginFactory.getToken();
 		$scope.user = LoginFactory.getUser();
+
+		//var response = EvalFactory.createEvaluation();
+		EvalFactory.getEvaluations().then(function(data){
+			console.log(data);
+		}).catch(function(errorMessage){
+			console.log('Error: ' + errorMessage);
+		});
+		//var response = EvalFactory.getEvaluation(2);
+		//console.log(response);
 
 		$scope.test = "test";
 	}
--- src/controllers/login.js
+++ src/controllers/login.js
@@ -7,26 +7,20 @@ app.controller("LoginController", [
 
 		$scope.login = function(){
 
-			$http.defaults.headers.common.Authorization = "Basic " + LoginFactory.getToken();
+			//$http.defaults.headers.common.Authorization = "Basic " + LoginFactory.getToken();
 			LoginFactory.login($scope.user.username, $scope.user.password).then(function(data){
-				//token = data[0];
-				//user = data[1]; 
-				//console.log("Token is: " + data[0]);
 				token = data[0];
 				user = data[1];
 				if(data[1].role === 'admin'){
 					$location.path('/admin');
 				}
 				else if(data[1].role === 'student'){
-					$location.path('/home');
+					$location.path('/student');
 				}
 				else{
 					$location.path('/');
 				}
-				//console.log("Username is: " + user.username);
 			});
-			//console.log(token);
-			//console.log(user);
 		};
 
 		$scope.test = "test";
